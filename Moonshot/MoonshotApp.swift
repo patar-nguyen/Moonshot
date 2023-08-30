9d43188e6ec84df26a704077564ee690f17f62dd
Patrick Nguyen <patar.nguyen@gmail.com>
2023-08-30 18:29:40 -0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Patrick Nguyen on 2023-08-30.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
